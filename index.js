0350111094a89a182b3cd7821401c5d09af91559
silentscribe <seelive.i@gmail.com>
2019-01-15 02:13:58 +0000

    request using request working without extra user comments

--- index.js
+++ index.js
@@ -8,11 +8,13 @@
 const request = require('request');
 const fs = require('fs');
 
-request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
-       .on('error', function (err) {                                   // Note 2
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+       .on('error', function (err) {
          throw err;
        })
-       .on('response', function (response) {                           // Note 3
+       .on('response', function (response) {
          console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Status Message: ', response.statusMessage);
+         console.log('Content Type: ', response.headers['content-type']);
        })
-       .pipe(fs.createWriteStream('./downloaded.html'));               // Note 4
+       .pipe(fs.createWriteStream('./future.jpg'));
